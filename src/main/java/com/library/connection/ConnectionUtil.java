8c933586bf44c1f1d1fc53c232199251bec83fff
SuriyakumarVijayarajan <suriyakumarsuriya12@gmail.com>
2022-01-30 10:58:21 +0530

    Update on Library Features

--- src/main/java/com/library/connection/ConnectionUtil.java
+++ src/main/java/com/library/connection/ConnectionUtil.java
@@ -1,15 +1,14 @@
 package com.library.connection;
 import java.sql.Connection;
 
-
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-	private static String password = "oracle";
+	
 	public static Connection getDBConnect() throws ClassNotFoundException, SQLException {
 		Class.forName("oracle.jdbc.OracleDriver");
-		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system",password);
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
 		
 	}
 
