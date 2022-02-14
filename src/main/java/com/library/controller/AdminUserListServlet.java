4206e0ac6fb348ca797adfefe6286fca96e23406
SuriyakumarVijayarajan <suriyakumarsuriya12@gmail.com>
2022-02-14 09:57:17 +0530

    Library Features Updated

--- src/main/java/com/library/controller/AdminUserListServlet.java
+++ src/main/java/com/library/controller/AdminUserListServlet.java
@@ -29,7 +29,7 @@ public class AdminUserListServlet extends HttpServlet {
 		try {
 			userList = user.userList();
 		} catch (SQLException e) {
-			e.printStackTrace();
+			e.getMessage();
 		}
 		request.setAttribute("adminUserList", userList);
 		RequestDispatcher rd=request.getRequestDispatcher("userList.jsp");
